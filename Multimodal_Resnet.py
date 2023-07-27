7ae7c6c92347de9a54357378a8e304890776dc16
RichardP789 <109404267+RichardP789@users.noreply.github.com>
2023-07-27 12:39:17 -0600

    Update and rename twelve_cat_resnet_extra.py to Multimodal_Resnet.py

--- Multimodal_Resnet.py
+++ Multimodal_Resnet.py
@@ -1,3 +1,4 @@
+#importing necessary libraries
 from PIL import Image
 import requests
 from io import BytesIO
@@ -16,7 +17,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Flatten
 from keras.optimizers import Adam
 
-import re
+#function to preprocess text
 def preprocess(text):
   text = str(text)
   text = text.lower()
@@ -35,9 +36,10 @@ def preprocess(text):
   text = text.strip(' ')
   return text
 
-
+#Path to images and metadata csv
 path = 'C:\\Users\\Richard\\Desktop\\ML-AI code bootcamp\\Final files\\Final image'
 
+#Categories to sort by 
 category = ["Africana Studies",
 "American Studies",
 "Arabic Studies",
@@ -51,10 +53,13 @@ category = ["Africana Studies",
 "Sociology",
 "Women Gender Sexuality Studies"]
 
+#reading in the csv from the path
 pl_csv = pd.read_csv('C:\\Users\\Richard\\Desktop\\ML-AI code bootcamp\\Final files\\Final image\\events_edited.csv', index_col=0)
 
+#setting the path of the directory with all the files
 dir_list = os.listdir(path)
 
+#Finding and mapping each image in the file path to the labels
 match_resource = []
 read_list = []
 
@@ -64,6 +69,7 @@ for i in dir_list:
         read_list.append(f'C:\\Users\\Richard\\Desktop\\ML-AI code bootcamp\\Final files\\Final image\\{i}')
         match_resource.append(int(j))
 
+#Updating the dataframe so it containes the image paths connected with each label
 pl_csv['Image'] = np.NaN
 
 for i in match_resource:
@@ -76,51 +82,72 @@ for i in match_resource:
 cat_img_df = pl_csv.dropna()
 cat_img_df = cat_img_df[['Mapped_Category', 'Image', 'Subject']]
 
-
+#Optaining the X(image) and Y(label) for each image
 y = cat_img_df['Mapped_Category']
 X = cat_img_df['Image']
+
+#Getting the additional features (keywords) from the dataset
 metadata_features = cat_img_df['Subject']
 metadata_features = metadata_features.apply(preprocess)
 metadata_features = list(metadata_features)
 
+#Vectorizing the labels
 y = pd.get_dummies(y, drop_first = False)
 
+#function to preprocess the images to the right size
 def load_and_preprocess_image(image_path, target_size):
     image = cv2.imread(image_path)
     image = cv2.resize(image, target_size)
     return image
-
+  
+#The target size
 target_size = (224, 224)
 
+#fitting all the images in X to the right size
 X = np.array([load_and_preprocess_image(path, target_size) for path in X])
 
+#Getting the train test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42)
 
+#Loading the vectorizer
 vectorizer = CountVectorizer()
 
+#Vectorizing the additional features
 metadata_features = vectorizer.fit_transform(metadata_features).toarray()
+
+#Setting up the image and additional features input
 image_input = Input(shape=(224, 224, 3))
 metadata_input = Input(shape=(metadata_features.shape[1],))
 
+#Setting up the ResNet50 model
 resNet_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
 
+#Freezing some layers so they don't get updated during training
 for layer in resNet_model.layers:
     layer.trainable = False
 
+#Getting the image features from the resnet model
 resnet_output = resNet_model(image_input)
 
+#flattening the output features
 flatten_output = Flatten()(resnet_output)
 
+#adding the additional features to the image features
 concatenated_features = Concatenate()([flatten_output, metadata_input])
 
+#dense layer with all the features
 x = Dense(512, activation='relu')(concatenated_features)
+#dense layer outputting to predictions
 predictions = Dense(len(category), activation='sigmoid')(x)
 
+#Setting up the model
 model = Model(inputs=[image_input, metadata_input], outputs=predictions)
 
+#Compiling the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
+#fitting the model on images, labels, and additional features
 model.fit([X_train, metadata_features[:X_train.shape[0]]], y_train, batch_size=32, epochs=10, validation_data=([X_test, metadata_features[X_train.shape[0]:]], y_test))
 
-
+#saving the model
 model.save('twelve_cat_resnet_extra.h5')
