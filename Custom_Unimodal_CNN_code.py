f374bf81c25d9fe0cc427b57721a48ba636afecf
RichardP789 <109404267+RichardP789@users.noreply.github.com>
2023-07-27 11:43:58 -0600

    Update and rename Custom_CNN.py to Custom_Unimodal_CNN_code.py

--- Custom_Unimodal_CNN_code.py
+++ Custom_Unimodal_CNN_code.py
@@ -1,4 +1,4 @@
-
+#importing necessary libraries
 from PIL import Image
 import requests
 from io import BytesIO
@@ -12,8 +12,10 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from keras.optimizers import Adam
 
+#Path to the images and metadata csv
 path = 'C:\\Users\\Richard\\Desktop\\ML-AI code bootcamp\\Final files\\Final image'
 
+#Categories to sort by 
 category = ["Africana Studies",
 "American Studies",
 "Arabic Studies",
@@ -27,10 +29,13 @@ category = ["Africana Studies",
 "Sociology",
 "Women Gender Sexuality Studies"]
 
+#reading in the csv from the path
 pl_csv = pd.read_csv('C:\\Users\\Richard\\Desktop\\ML-AI code bootcamp\\Final files\\Final image\\events_edited.csv', index_col=0)
 
+#setting the path of the directory with all the files
 dir_list = os.listdir(path)
 
+#Finding and mapping each image in the file path to the labels
 match_resource = []
 read_list = []
 
@@ -40,6 +45,7 @@ for i in dir_list:
         read_list.append(f'C:\\Users\\Richard\\Desktop\\ML-AI code bootcamp\\Final files\\Final image\\{i}')
         match_resource.append(int(j))
 
+#Updating the dataframe so it containes the image paths connected with each label
 pl_csv['Image'] = np.NaN
 
 for i in match_resource:
@@ -52,24 +58,27 @@ for i in match_resource:
 cat_img_df = pl_csv.dropna()
 cat_img_df = cat_img_df[['Mapped_Category', 'Image']]
 
-
+#Optaining the X(image) and Y(label) for each image
 y = cat_img_df['Mapped_Category']
 X = cat_img_df['Image']
 
+#Vectorizing the labels
 y = pd.get_dummies(y, drop_first = False)
 
+#function to process the images to the right size
 def load_and_preprocess_image(image_path, target_size):
     image = cv2.imread(image_path)
     image = cv2.resize(image, target_size)
     return image
-
+#The target size
 target_size = (224, 224)
-
+#fitting all the images in X to the right size
 X = np.array([load_and_preprocess_image(path, target_size) for path in X])
 
+#Getting the train test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42)
 
-
+#creating the seqential model
 model = Sequential()
 
 model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)))
@@ -86,7 +95,7 @@ model.add(Flatten())
 model.add(Dense(256, activation='relu'))
 model.add(Dense(len(category), activation='sigmoid'))  
 
-# Step 5: Compile and fit the model
+#Compile and fit the model
 model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy', metrics=['accuracy'])
 
 batch_size = 32
@@ -94,5 +103,5 @@ epochs = 10
 
 model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_test, y_test))
 
-
+#saving the trained model
 model.save('twelve_cat_CNN.h5')
